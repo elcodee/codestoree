df4fd358e690369986bea905e71855946e155707
Rama Aditya <46771792+elcodee@users.noreply.github.com>
2022-11-12 20:26:34 +0700

    V1

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    swcMinify: true,
-    async redirects() {
-        return [
-          {
-            source: '/blog',
-            destination: '/',
-            permanent: true,
-          },
-        ]
-      },
+  swcMinify: false,
+  async redirects() {
+      return [
+        {
+          source: '/blog',
+          destination: '/',
+          permanent: true,
+        },
+      ]
+    },
 };
